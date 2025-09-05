dd776380e72e9822f5b93d4942853d1f9de2b459
Artasi67 <athurtavaressilva@gmail.com>
2025-09-05 09:15:14 -0300

    Vamo ve se essa bagaça funciona

--- Controllers/ProdutoController.cs
+++ Controllers/ProdutoController.cs
@@ -3,6 +3,7 @@ using catalogo_produto.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System.Security.Claims;
 
 namespace catalogo_produto.Controllers
@@ -10,12 +11,13 @@ namespace catalogo_produto.Controllers
     public class ProdutoController : Controller
     {
         private readonly DbConfig _dbConfig;
-        public ProdutoController(DbConfig dbConfig) 
+
+        public ProdutoController(DbConfig dbConfig)
         {
             _dbConfig = dbConfig;
         }
 
-        public ActionResult Index () 
+        public ActionResult Index()
         {
             return View();
         }
@@ -27,7 +29,18 @@ namespace catalogo_produto.Controllers
         }
 
         [Authorize]
-        public async Task<ActionResult> Cadastrar() 
+        public async Task<ActionResult> ListarPorUsuarioId()
+        {
+            var usuario_id = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var produtos = await _dbConfig.produtos
+                .Where(p => p.Id_produto == int.Parse(usuario_id))
+                .ToListAsync();
+
+            return View(produtos);
+        }
+
+        [Authorize]
+        public async Task<ActionResult> Cadastrar()
         {
             var categorias = await _dbConfig.categorias.ToListAsync();
             return View(categorias);
@@ -37,11 +50,46 @@ namespace catalogo_produto.Controllers
         public async Task<ActionResult> Salvar(Produto produto)
         {
             var usuario_id = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            if(usuario_id != null) 
+            if (usuario_id != null)
             {
                 produto.Id_usuario = int.Parse(usuario_id);
             }
+            produto.Data_registro_produto = new DateTime();
+
+            _dbConfig.produtos.Add(produto);
+            await _dbConfig.SaveChangesAsync();
+            return RedirectToAction("Listar");
         }
 
+        [Authorize]
+        public async Task<ActionResult> Editar(int id)
+        {
+            var produto = await _dbConfig.produtos
+                .Include(p => p.usuario)
+                .Include(p => p.categoria)
+                .FirstOrDefaultAsync(
+                    p => p.Id_produto == id
+                );
+            return View(produto);
+        }
+
+        [Authorize]
+        public async Task<ActionResult> Atualizar(Produto produto)
+        {
+            _dbConfig.produtos.Update(produto);
+            await _dbConfig.SaveChangesAsync();
+
+            return RedirectToAction("Listar");
+        }
+
+        [Authorize]
+        public async Task<ActionResult> Deletar(int id)
+        {
+            var produto = await _dbConfig.produtos.FindAsync(id);
+            _dbConfig.produtos.Remove(produto);
+            await _dbConfig.SaveChangesAsync();
+
+            return RedirectToAction("Listar");
+        }
     }
 }
